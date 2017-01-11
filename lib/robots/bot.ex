8fe50f05d22ea12df3027d6df01804d4387acd69
Will Lewis <will@wflewis.com>
2017-01-10 21:18:31 -0500

    Elixir meetup progress on implementing Robots.Bot

--- lib/robots/bot.ex
+++ lib/robots/bot.ex
@@ -1,2 +1,32 @@
 defmodule Robots.Bot do
+  defstruct x: 0, y: 0, name: "Bot", speed: 1
+
+  def new do
+    %Robots.Bot{}
+  end
+
+  def new(name: name) do
+    Map.put(new, :name, name)
+  end
+
+  def new(speed: speed) do
+    Map.put(new, :speed, speed)
+  end
+
+  def y(bot) do
+    bot.y
+  end
+
+  def x(bot) do
+    bot.x
+  end
+
+  def speed(bot) do
+    bot.speed
+  end
+
+  def greetings(bot) do
+    "Hey, my name is #{bot.name}"
+  end
+
 end
